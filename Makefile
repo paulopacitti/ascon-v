dd1333add7d773a0deb19572c1b136334f2ea4b2
paulopacitti <ppacitti@outlook.com>
2023-10-28 12:19:51 -0300

    Add opt64 implementation as well added speedup information to the benchmark

--- Makefile
+++ Makefile
@@ -13,11 +13,14 @@ all: benchmark test
 test: tests/aead_test.c $(BUILD)/asconv.o $(BUILD)/ref.o $(BUILD)/Unity.o
 	$(CC) $^ $(CFLAGS) -o $(BUILD)/test
 
-benchmark: benchmark/benchmark.c $(BUILD)/ref.o $(BUILD)/asconv.o
+benchmark: benchmark/benchmark.c $(BUILD)/ref.o $(BUILD)/opt64.o $(BUILD)/asconv.o
 	$(CC) $^ $(CFLAGS) -o $(BUILD)/benchmark
 
 $(BUILD)/ref.o: $(BUILD) ref/aead.c
 	$(CC) $(CFLAGS) -c ref/aead.c -o $(BUILD)/ref.o
+
+$(BUILD)/opt64.o: $(BUILD) opt64/aead.c
+	$(CC) $(CFLAGS) -c opt64/aead.c -o $(BUILD)/opt64.o
 
 $(BUILD)/asconv.o: src/asconv.c src/asconv.h $(BUILD)
 	$(CC) $(CFLAGS) -c src/asconv.c -o $(BUILD)/asconv.o
--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -1,10 +1,12 @@
+#include "../opt64/crypto_aead.h"
 #include "../ref/crypto_aead.h"
 #include "../src/asconv.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 #define REFERENCE_IMPLEMENTATION 0
-#define ASCONV_IMPLEMENTATION    1
+#define OPT64_IMPLEMENTATION     1
+#define ASCONV_IMPLEMENTATION    2
 
 const int ITERATIONS = 2000;
 const int RESOLUTION = 45;   // 45 ns on T-Head C906;
@@ -86,6 +88,21 @@ void benchmark_encryption(int method, unsigned long long *time,
         *cycles = clock_count(time_elapsed);
 
         return;
+
+    case OPT64_IMPLEMENTATION:
+        for (int i = 0; i < ITERATIONS; i++) {
+            ref_timer_start = rdtime();
+            crypto_aead_encrypt_opt64(ct, &clen, msg, mlen, ad, adlen,
+                                      (void *) 0, nonce, key);
+            ref_timer_end = rdtime();
+            time_counts[i] = ref_timer_end - ref_timer_start;
+        }
+
+        time_elapsed = average_time_elapsed(time_counts, ITERATIONS);
+        *time = time_elapsed;
+        *cycles = clock_count(time_elapsed);
+
+        return;
     case ASCONV_IMPLEMENTATION:
         for (int i = 0; i < ITERATIONS; i++) {
             ref_timer_start = rdtime();
@@ -122,6 +139,20 @@ void benchmark_decryption(int method, unsigned long long *time,
         *cycles = clock_count(time_elapsed);
 
         return;
+    case OPT64_IMPLEMENTATION:
+        for (int i = 0; i < ITERATIONS; i++) {
+            ref_timer_start = rdtime();
+            result |=
+                crypto_aead_decrypt_opt64(decrypted_msg, &decrypted_mlen, NULL,
+                                          ct, clen, ad, adlen, nonce, key);
+            ref_timer_end = rdtime();
+            time_counts[i] = ref_timer_end - ref_timer_start;
+        }
+        time_elapsed = average_time_elapsed(time_counts, ITERATIONS);
+        *time = time_elapsed;
+        *cycles = clock_count(time_elapsed);
+
+        return;
     case ASCONV_IMPLEMENTATION:
         for (int i = 0; i < ITERATIONS; i++) {
             ref_timer_start = rdtime();
@@ -138,54 +169,112 @@ void benchmark_decryption(int method, unsigned long long *time,
 }
 
 int main() {
-    int message_sizes[4] = {8, 32, 64, 1000};
-    unsigned long long encryption_ref_time_ns[4];
-    unsigned int encryption_ref_cycles[4];
-    unsigned long long encryption_asconv_time_ns[4];
-    unsigned int encryption_asconv_cycles[4];
-    unsigned long long decryption_ref_time_ns[4];
-    unsigned int decryption_ref_cycles[4];
-    unsigned long long decryption_asconv_time_ns[4];
-    unsigned int decryption_asconv_cycles[4];
+    int message_sizes[5] = {8, 32, 64, 1000, 4000};
+    unsigned long long encryption_ref_time_ns[5];
+    unsigned int encryption_ref_cycles[5];
+    unsigned long long encryption_opt64_time_ns[5];
+    unsigned int encryption_opt64_cycles[5];
+    unsigned long long encryption_asconv_time_ns[5];
+    unsigned int encryption_asconv_cycles[5];
+
+    unsigned long long decryption_ref_time_ns[5];
+    unsigned int decryption_ref_cycles[5];
+    unsigned long long decryption_opt64_time_ns[5];
+    unsigned int decryption_opt64_cycles[5];
+    unsigned long long decryption_asconv_time_ns[5];
+    unsigned int decryption_asconv_cycles[5];
 
     msg = malloc(sizeof(unsigned char));
     ad = malloc(sizeof(unsigned char));
     ct = malloc(sizeof(unsigned char));
     decrypted_msg = malloc(sizeof(unsigned char));
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < 5; i++) {
         benchmark_init(message_sizes[i], 16);
         benchmark_encryption(REFERENCE_IMPLEMENTATION,
                              &encryption_ref_time_ns[i],
                              &encryption_ref_cycles[i]);
+        benchmark_encryption(OPT64_IMPLEMENTATION, &encryption_opt64_time_ns[i],
+                             &encryption_opt64_cycles[i]);
         benchmark_encryption(ASCONV_IMPLEMENTATION,
                              &encryption_asconv_time_ns[i],
                              &encryption_asconv_cycles[i]);
         benchmark_decryption(REFERENCE_IMPLEMENTATION,
                              &decryption_ref_time_ns[i],
                              &decryption_ref_cycles[i]);
+        benchmark_decryption(OPT64_IMPLEMENTATION, &decryption_opt64_time_ns[i],
+                             &decryption_opt64_cycles[i]);
         benchmark_decryption(ASCONV_IMPLEMENTATION,
                              &decryption_asconv_time_ns[i],
                              &decryption_asconv_cycles[i]);
     }
 
-    printf("%-15s%-25s%-20s%-15s%-10s%-10s\n", "[Algorithm]",
-           "[Message size (B)]", "[Implementation]", "[Operation]", "[Cycles]",
-           "[Time (s)]");
+    /* Performance table */
+    printf("Performance table:\n");
+    printf("___________________________________________________________________"
+           "__________________________________\n");
+    printf("%-15s%-20s%-12s%-18s%-10s%-15s%-10s\n", "[Algorithm]",
+           "[Message size (B)]", "[Operation]", "[Implementation]", "[Cycles]",
+           "[Cycles/B]", "[Time (s)]");
     for (int i = 0; i < 4; i++) {
-        printf("%-15s%-25d%-20s%-15s%-10u%-10.9f\n", "Ascon128",
-               message_sizes[i], "reference", "encryption",
-               encryption_ref_cycles[i], encryption_ref_time_ns[i] * NS);
-        printf("%-15s%-25d%-20s%-15s%-10u%-10.9f\n", "Ascon128",
-               message_sizes[i], "asconv", "encryption",
-               encryption_asconv_cycles[i], encryption_asconv_time_ns[i] * NS);
-        printf("%-15s%-25d%-20s%-15s%-10u%-10.9f\n", "Ascon128",
-               message_sizes[i], "reference", "decryption",
-               decryption_ref_cycles[i], decryption_ref_time_ns[i] * NS);
-        printf("%-15s%-25d%-20s%-15s%-10u%-10.9f\n", "Ascon128",
-               message_sizes[i], "asconv", "decryption",
-               decryption_asconv_cycles[i], decryption_asconv_time_ns[i] * NS);
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "encryption", "reference",
+               encryption_ref_cycles[i],
+               encryption_ref_cycles[i] / message_sizes[i],
+               encryption_ref_time_ns[i] * NS);
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "encryption", "opt64",
+               encryption_opt64_cycles[i],
+               encryption_opt64_cycles[i] / message_sizes[i],
+               encryption_opt64_time_ns[i] * NS);
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "encryption", "asconv",
+               encryption_asconv_cycles[i],
+               encryption_asconv_cycles[i] / message_sizes[i],
+               encryption_asconv_time_ns[i] * NS);
+
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "decryption", "reference",
+               decryption_ref_cycles[i],
+               decryption_ref_cycles[i] / message_sizes[i],
+               decryption_ref_time_ns[i] * NS);
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "decryption", "opt64",
+               decryption_opt64_cycles[i],
+               decryption_opt64_cycles[i] / message_sizes[i],
+               decryption_opt64_time_ns[i] * NS);
+        printf("%-15s%-20d%-12s%-18s%-10u%-15u%-10.9f\n", "Ascon128",
+               message_sizes[i], "decryption", "asconv",
+               decryption_opt64_cycles[i],
+               decryption_asconv_cycles[i] / message_sizes[i],
+               decryption_asconv_time_ns[i] * NS);
     }
+    printf("___________________________________________________________________"
+           "__________________________________\n");
+
+    /* Speedup results */
+    printf("Speedup results (with a message size of 4MB):\n");
+    printf("%-20s%-30s%-30s%-30s%-30s\n", "[Implementation]",
+           "[Encryption (clock cycles)]", "[Decryption (clock cycles)]",
+           "[Encryption relative speed]", "[Decryption relative speed]");
+    printf("%-20s%-30d%-30d%-30.2f%-30.2f\n", "reference",
+           encryption_ref_cycles[4], decryption_ref_cycles[4],
+           (float) ((float) encryption_ref_cycles[4] /
+                    (float) encryption_ref_cycles[4]),
+           (float) ((float) decryption_ref_cycles[4] /
+                    (float) decryption_ref_cycles[4]));
+    printf("%-20s%-30d%-30d%-30.2f%-30.2f\n", "opt64",
+           encryption_opt64_cycles[4], decryption_opt64_cycles[4],
+           (float) ((float) encryption_ref_cycles[4] /
+                    (float) encryption_opt64_cycles[4]),
+           (float) ((float) decryption_ref_cycles[4] /
+                    (float) decryption_opt64_cycles[4]));
+    printf("%-20s%-30d%-30d%-30.2f%-30.2f\n", "asconv",
+           encryption_asconv_cycles[4], decryption_asconv_cycles[4],
+           (float) ((float) encryption_ref_cycles[4] /
+                    (float) encryption_asconv_cycles[4]),
+           (float) ((float) decryption_ref_cycles[4] /
+                    (float) decryption_asconv_cycles[4]));
 
     free(ct);
     free(ad);
